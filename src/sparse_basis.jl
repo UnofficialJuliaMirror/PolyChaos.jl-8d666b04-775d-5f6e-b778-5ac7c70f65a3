2e4c6b8d9d894db2b58b9ddeb807e0723e41be37
Rajput <unksa@student.kit.edu>
2019-09-09 11:38:56 +0200

    the optimum values of epsilon's and accuracy have been set here

--- src/sparse_basis.jl
+++ src/sparse_basis.jl
@@ -1,5 +1,4 @@
-export orthosparse, orthosparseMulti
-using LinearAlgebra
+export orthosparse
 
 function regress(M,y)
   c = pinv(M)*y
@@ -29,13 +28,14 @@ function orthosparse(y::Vector{Float64},x::Vector{Float64},name::String,p_max::I
     R2 = 1 - lack_of_fit(y,ymod)
     for i in p_index
         i >= p_max && break
-        ######################## FORWARD STEP ###########################
+
+        ##### FORWARD STEP
         display("stat of fwd step")
         push!(A,i)
         Φ1 = evaluate(A,x,op)
         a_hat, ymod = regress(Φ1,y)
         @show R2_new = 1 - lack_of_fit(y,ymod)
-        # if R2_new is significantlz better, then push to the basis
+        # if R2_new is significantly better, then push to the basis
         if abs(R2_new - R2) >= ε_forward
             R2 = R2_new
             # do nothing
@@ -47,8 +47,7 @@ function orthosparse(y::Vector{Float64},x::Vector{Float64},name::String,p_max::I
         @show A_plus = A
         display("End of Forward step")
 
-        ######################### Backward STEP #########################
-
+        ##### Backward STEP
         if A_plus[end] == i
             display("bwd step")
             for b in A_plus[1:end-1]
@@ -65,50 +64,47 @@ function orthosparse(y::Vector{Float64},x::Vector{Float64},name::String,p_max::I
             display("End of Backward Step")
         end
 
-        ################ CHECK TERMINATION CRITERION ###################
+        ##### CHECK TERMINATION CRITERION
         if length(A) != 0
           Φ3 = evaluate(A,x,op)
           a_tmp, ymod = regress(Φ3,y)
           @show R2_accuracy = 1 - lack_of_fit(y,ymod)
           print("\n")
-            if R2_accuracy >= accuracy
+            if R2 >= accuracy
                 println("Accuracy achieved. Breaking.\n")
                 return a_tmp, A
             end
         end
-        #################################################################
+        #####
     end
     error("Algorithm terminated early; perhaps a pathological problem was provided.")
 end
 
-function orthosparseMulti(y::Array{Float64,1},x::Matrix{Float64},name::Array{String,1},p_max::Int64,numu::Int64,ε_forward::Float64,ε_backward::Float64,accuracy::Float64)
+function orthosparse(y::Array{Float64,1},x::Matrix{Float64},name::Array{String,1},p_max::Int64,numu::Int64; ε_forward::Float64=1e-12 , ε_backward::Float64=1e-12 , accuracy::Float64=1.0-1e-15)
     p_index = 1:p_max
     j_index = 1:numu
 
-    ####### Generates Multivariate ortho Polynomials
+    ##### Generates Multivariate ortho Polynomials
     ops = OrthoPoly[]
     for j = 1:length(name)
         push!(ops,OrthoPoly(name[j],p_max))
     end
     mop = MultiOrthoPoly(ops,p_max) # Multivariate Orthogonal Polynomials
 
-    #####
-    A = Array{Array{Int64,1},1}();
-    A_plus = Array{Array{Int64,1},1}();
+    A = Array{Array{Int64,1},1}(); # Index set
+    A_plus = Array{Array{Int64,1},1}(); # Index set for forward step
     push!(A,vec(zeros(Int64,1,numu)));
     A = vector_to_matrix(A);
     R2 = 0.0;
-    #####
-
     Φ0 = evaluate(A,x,mop)
     Φ0 = transpose(Φ0)
     a_tmp,ymod = regress(Φ0,y)
-    @show R2 = 1 - lack_of_fit(y,ymod)
+    R2 = 1 - lack_of_fit(y,ymod)
     A = matrix_to_vector(A);
     for p in p_index
         p >= p_max && break
 
-        ########################### FORWARD STEP ###############################
+        ####### FORWARD STEP
 
         ##### Generating the set multiindecies of pth order
         I_p = Array{Array{Int64,1},1}();
@@ -118,53 +114,39 @@ function orthosparseMulti(y::Array{Float64,1},x::Matrix{Float64},name::Array{Str
                 push!(I_p,Vector(k))
             end
         end
-        display("I_p set")
-        display(I_p)
-        println("\n")
-        ############
-
+        ####
         for j in I_p
             push!(A,Vector(j))
             A = vector_to_matrix(A);
-            #display(A)
             Φ1 = evaluate(A,x,mop)
             Φ1 = transpose(Φ1)
             a_hat, ymod = regress(Φ1,y)
-            @show R2_new = 1 - lack_of_fit(y,ymod)
+            R2_new = 1 - lack_of_fit(y,ymod)
             A = matrix_to_vector(A);
-
-            # if R2_new is significantlz better, then push to the basis
-            if R2_new == 1.0
+            # if R2_new is significantly better, then push to the basis/Index set
+            if R2_new == 1.0 # do nothing
                 R2 = R2_new
-                # do nothing
-            elseif abs(R2_new - R2) > ε_forward
+            elseif abs(R2_new - R2) > ε_forward # do nothing
                 R2 = R2_new
-                # do nothing
             else
-                display("filter in fwd step")
                 pop!(A)
             end
         end
-        # A_plus = A;
-        display(A)
-        display("End of Forward step")
-        println("\n")
-        ########################## Backward STEP ###############################
 
-        @show A_a = A[[sum(x) == p for x in A]]; #set of pth order basis
-        @show A_b = A[[sum(x) < p for x in A]]; #set of all basis of order less than p
+        ##### Backward STEP
+        A_a = A[[sum(x) == p for x in A]]; #set of pth order basis
+        A_b = A[[sum(x) < p for x in A]]; #set of all basis of order less than p
         if A_a != []
             for s in A_b
                 A_plus = A[[x != s for x in A]];
-                @show A_plus = vector_to_matrix(A_plus);
+                A_plus = vector_to_matrix(A_plus);
                 Φ2 = evaluate(A_plus,x,mop);
                 Φ2 = transpose(Φ2);
                 a_tmp, ymod = regress(Φ2,y);
                 r2_new = 0.0;
-                @show r2 = 1 - lack_of_fit(y,ymod);
+                r2 = 1 - lack_of_fit(y,ymod);
                 A_plus = matrix_to_vector(A_plus);
                 if abs(R2 - r2) < ε_backward || r2 == 1.0
-                    # display("filter in bwd step")
                     A = A_plus;
                 else
                     push!(A_plus,Vector(s))
@@ -173,27 +155,26 @@ function orthosparseMulti(y::Array{Float64,1},x::Matrix{Float64},name::Array{Str
             A = A_plus
         end
         A = vector_to_matrix(A);
-        display("A")
-        display(A)
-        display("End of Backward step")
-        println("\n")
 
-        ##################### CHECK TERMINATION CRITERION ######################
-
+        ##### CHECK TERMINATION CRITERION
         if size(A)[1]  != 0
-            display("check termination")
             Φ3 = evaluate(A,x,mop)
             Φ3 = transpose(Φ3)
             a_tmp, ymd = regress(Φ3,y)
-            @show r2_accuracy = 1 - lack_of_fit(y,ymd)
-            println("\n")
+            R2 = 1 - lack_of_fit(y,ymd)
             A = matrix_to_vector(A);
-            if r2_accuracy >= accuracy
-                println("Accuracy achieved. Breaking.\n")
+            if R2 >= accuracy || isapprox(R2,accuracy,atol=1e-15)
+                temp_ind = findall(x->x>1e-8,a_tmp)
+                A = A[temp_ind]
+                a_tmp = a_tmp[temp_ind]
+                println("R2: ", R2)
+                println("coeff: ", a_tmp)
+                println("indices: ", A)
+                println("Accuracy achieved. Breaking\n")
                 return a_tmp,A
             end
         end
-        ########################################################################
+        ######
     end
     error("Algorithm terminated early; perhaps a pathological problem was provided.")
 end
